34bae355e3a56e10eea61d5795e8f863a865ffef
ORE0000 <ashutoshpant.855@gmail.com>
2024-11-22 17:10:29 +0530

    update

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,9 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
-  optimizeDeps: {
-    include: ['@google/generative-ai'],
-  }
+  build: {
+    rollupOptions: {
+      external: ['@google/generative-ai'], // Exclude from bundling
+    },
+  },
 });
